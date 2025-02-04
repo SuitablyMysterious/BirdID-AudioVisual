91977911ce2d59a28ad539a1572535d35de76f08
SuitablyMysterious <fly2be@outlook.com>
2025-02-04 18:18:55 +0000

    Update requirements and add audio analysis functionality

---
+++ source/.analysis/analyseFunctions.py
@@ -0,0 +1,14 @@
+from birdnet.models import ModelV2M4
+from pathlib import Path
+
+# Crate model instance
+currentModel = ModelV2M4()
+
+def analyseAudio(path,latitude,longitude,week):
+    species_in_area = model.predict_species_at_location_and_time(latitude,longitude,week)
+    predictions = model.predict_species_within_audio_file(
+        Path(path),
+        filter_species=set(species_in_area.keys())
+    )
+
+
